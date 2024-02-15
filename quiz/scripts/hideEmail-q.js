78d85e4ac7e5e3d64c1b5b2fed6b000d6e9564ff
Hasan Rawjee <105398848+hasanrawjee@users.noreply.github.com>
2024-02-15 15:04:43 -0500

    Array & map deep exercise

--- quiz/scripts/hideEmail-q.js
+++ quiz/scripts/hideEmail-q.js
@@ -5,7 +5,18 @@ const emails = ['mary@northeastern.edu', 'ari@khoury.northeastern.edu', 'jk_neu.
 window.onload = function hideEmail() {
     const list = document.getElementById('emails')
     list.innerHTML = '';
+    result = []
     for (const email of emails) {
         // complete the loop
+        if (isValidEmail(email)) {
+            hiddenEmail = email.split('@')[0].replace(/./g, '*') + '@' + email.split('@')[1];
+            const liElement = document.createElement('li');
+            liElement.innerText = hiddenEmail;
+            ulElement.append(liElement);
+        }
     }
+}
+
+function isValidEmail(email) {
+    return /^[a-zA-Z_0-9]+@(.+\.)*northeastern\.edu/.test(email)
 }
